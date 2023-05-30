de6b56b16049464a3eac12b10d9805e4459a3115
Victor <victoremanuel.psi@gmail.com>
2023-05-30 19:33:09 -0300

    Adiciona um estado base

--- main.js
+++ main.js
@@ -5,4 +5,13 @@ form.addEventListener('submit', envia);
 function envia(event){
     event.preventDefault();
     console.log("Form enviado");
+    estado++
+    atualiza();
+}
+
+let estado=0;
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML=`<li> ${estado} </li>`;
 }
