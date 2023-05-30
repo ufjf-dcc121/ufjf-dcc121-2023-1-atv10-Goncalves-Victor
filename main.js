2d918d93eeecf382959e7c789d4a49bb2ce4839e
Victor <victoremanuel.psi@gmail.com>
2023-05-30 20:21:44 -0300

    Adiciona elementos no vetor

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 import store from "./store.js";
 
 const form = document.forms.entrada;
-
 form.addEventListener('submit', envia);
+
+
+atualiza();
 
 function envia(event){
     event.preventDefault();
     console.log("Form enviado");
-    store.estado++
+    const n=form.valor.value;
+    store.estado.push(n);
+    form.valor.value="";
+    form.valor.focus();
     atualiza();
 }
 
-atualiza();
-
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML=`<li> ${store.estado} </li>`;
+    ol.innerHTML="";
+    for(let i = 0 ;i<store.estado.length;i++){
+        const li = document.createElement('li');
+        li.textContent=store.estado[i];
+        ol.appendChild(li);
+        
+    }
 }
